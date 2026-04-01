f5c0f58e3465c38c49f5d9c9403064e464531572
Johnny Thunder <ajguerrero39@gmail.com>
2026-04-01 10:49:26 -0500

    Create defaults.js

---
+++ scripts/config/defaults.js
@@ -0,0 +1,10 @@
+export const DEFAULT_PLAYER_NAME = 'Garfield';
+
+export const DEFAULT_LEADERBOARD = [
+  { name: 'Anthony', score: 9000 },
+  { name: 'Julio', score: 7000 },
+  { name: 'Guerrero', score: 5000 },
+  { name: 'Juarez', score: 3000 }
+];
+
+export const GARFIELD_NEW_SCORE = 9500;
